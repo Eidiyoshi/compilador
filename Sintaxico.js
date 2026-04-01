7503d930991712b1ac1bc67b47de3b28c0f26050
eidiyoshi <eidiyoshi@hotmail.com>
2026-04-01 19:08:43 -0300

    declarar variavel feito

--- Sintaxico.js
+++ Sintaxico.js
@@ -1,5 +1,10 @@
+import { types, tokens } from "./Modulos/DefinicoesLexico.mjs";
+// globalizar para facilitar a recursão
+var pilha = [];
+var fila = [];
 
 // ver como ficar dps, pode acabar virando uma constante
+// talvez precise posteriormente para considerar todos os caminhos, mas no momento nao precisei
 function construirTabelaPreditiva(){
     let tabelaPreditiva = {};
     tabelaPreditiva["S"]["a"] = "ab";
@@ -18,27 +23,113 @@ function enfileirarTokens(arrayTokens){
     return fila;
 }
 
-function DeclararVariavel(pilha){
-    pilha.push("ponto_e_virgula")
-    pilha.push("identificador_valido")
-    pilha.push("tipo")
-    return pilha
+function declararErroSintaxico(erroString){
+    document.getElementById("checkSintaxico").textContent = erroString;
+}
+
+function DeclararVariavel(){
+    pilha.push("<ponto_e_virgula>")
+    pilha.push("<virgula>")
+    pilha.push("<identificador_valido>")
+    pilha.push("<tipo>")
+
+    var erro = 0;
+    var iteracoesMax = 0;
+    do {
+        iteracoesMax = iteracoesMax + 1;
+
+        var tokenAtual = fila.shift();
+        var topoAtual = pilha.pop();
+
+        console.log(topoAtual)
+        console.log(tokenAtual)
+        console.log("-----")
+
+        if (topoAtual == "<tipo>" && (types.includes(tokenAtual) )) continue;
+        
+        if (topoAtual == "<identificador_valido>" && tokenAtual == "identificador_valido" ) continue;
+
+        if (topoAtual == "<virgula>" && tokenAtual == "virgula" ){
+            pilha.push("<virgula>")
+            pilha.push("<identificador_valido>")
+            continue;
+        } 
+
+        if( topoAtual == "<virgula>" && tokenAtual == "ponto_e_virgula" ){ // esperando mais uma variavel, mas encontrou ponto_e_virgula, caminho normal
+            topoAtual = pilha.pop();
+            if( topoAtual == "<identificador_valido>" && tokenAtual == "ponto_e_virgula" ) topoAtual = pilha.pop();
+        }
+        
+
+        // ERROS
+
+        if (topoAtual == "<identificador_valido>" && tokenAtual == "virgula" ){
+            declararErroSintaxico("ERRO: NUMERO EXCESSIVO DE VIRGULAS")
+            console.log("ERRO: NUMERO EXCESSIVO DE VIRGULAS")
+            erro = 1;
+            break;
+        }
+
+        if (topoAtual == "<virgula>" && tokenAtual == "identificador_valido" ){
+            declararErroSintaxico("ERRO: NUMERO EXCESSIVO DE VIRGULAS")
+            console.log("ERRO: NUMERO EXCESSIVO DE VIRGULAS")
+            erro = 1;
+            break;
+        }
+
+        if( topoAtual != "<ponto_e_virgula>" && tokenAtual == "ponto_e_virgula"){ 
+            declararErroSintaxico("ERRO: PONTO_E_VIRGULA INESPERADO")
+            console.log("ERRO: PONTO_E_VIRGULA INESPERADO")
+            erro = 1;
+            break;
+        }
+        
+        if( (topoAtual == "<ponto_e_virgula>" || topoAtual == "<identificador_valido>" || topoAtual == "<virgula>" ) && !tokens.includes(tokenAtual) ){ 
+            declararErroSintaxico("ERRO: PONTO_E_VIRGULA NAO ENCONTRADO")
+            console.log("ERRO: PONTO_E_VIRGULA NAO ENCONTRADO")
+            erro = 1;
+            break;
+        }
+        
+        if (topoAtual == "<tipo>" && !tokens.includes(tokenAtual)  ){
+            declararErroSintaxico("ERRO: TIPO NAO ENCONTRADO")
+            console.log("ERRO: TIPO NAO ENCONTRADO")
+            erro = 1;
+            break;
+        }
+        
+        if( !tokens.includes(tokenAtual)  ){ 
+            declararErroSintaxico("ERRO: TOKEN NAO IDENTIFICADO")
+            console.log("ERRO: TOKEN NAO IDENTIFICADO")
+            erro = 1;
+            break;
+        }
+
+    } while ( (tokenAtual != "ponto_e_virgula" && topoAtual != "<ponto_e_virgula>" ) && (iteracoesMax < 100));
+
+    if(erro){
+        console.log("ERRO NA DECLARACAO DE VARIAVEL");
+    } 
 }
 
 export function analisadorSintaxico(arrayTokens){
-    var fila = enfileirarTokens(arrayTokens);
-    var pilha = [];
+    fila = enfileirarTokens(arrayTokens);
 
-    // eventualmente isto seria substituido por S pra programa ou algo do tipo
+    declararErroSintaxico("");
+
+    // eventualmente isto seria substituido por S start ou algo do tipo
     // testemos para declaracao de variavel no momento
-    pilha.push("Variavel");
+    pilha.push("<Variavel>");
     
 
-    while(fila.length != 1 && fila[0] != "$" ){
-        var tokenAtual = fila.shift()
+    while( (fila[0] != "$" ) && (fila.length > 0) ){
         var topoPilha = pilha.pop()
 
-        if( topoPilha == "Variavel" ) DeclararVariavel(pilha);
+        if( topoPilha == "<Variavel>" ) DeclararVariavel();
+
+        fila.shift()
+        
     }
-    construirTabelaPreditiva();
+    
+    //construirTabelaPreditiva();
 }
